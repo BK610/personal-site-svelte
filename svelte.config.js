8df6326352aad9838f563655db83fc8478d6e976
Bailey Kane <bailey.orion.kane@gmail.com>
2026-03-15 15:34:23 -0400

    build(svelte.config): add path aliases to svelte config file

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,13 @@ import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: { adapter: adapter() },
+  kit: {
+    adapter: adapter(),
+    alias: {
+      "@": "./src",
+      "@lib": "./src/lib",
+    },
+  },
   vitePlugin: {
     dynamicCompileOptions: ({ filename }) =>
       filename.includes("node_modules") ? undefined : { runes: true },
