0a7635ccad72930b850e1f97011a09d7240f6546
Bailey Kane <bailey.orion.kane@gmail.com>
2026-04-19 14:37:53 -0400

    refactor(navigation): use <a> for navigation instead of custom navigation select
    this actually works with view transitions

---
+++ src/lib/components/organisms/navigation.svelte
@@ -0,0 +1,15 @@
+<nav class="navigation">
+  <a href={"/"}>Home</a>
+  <a href={"/about"}>About</a>
+  <a href={"/projects"}>Projects</a>
+  <a href={"/books"}>Books</a>
+  <a href={"/recipes"}>Recipes</a>
+</nav>
+
+<style>
+  .navigation {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+  }
+</style>
